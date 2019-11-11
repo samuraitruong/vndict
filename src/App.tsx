8d853ef2bca1b28fcbc39aa9d00567189dec92f8
Truong Nguyen <samuraitruong@hotmail.com>
2019-11-11 22:50:07 +1100

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+}));
+
 
 const App: React.FC = () => {
+  const classes = useStyles();
+  const [data, setData] = useState({enViData: null,});
+  const [keyword, setKeyword] = useState("");
+  const search = async (inputKeyword: string) => {
+    const ft = await fetch(`https://samuraitruong.github.io/open-vn-en-dict/html/${inputKeyword}.json`);
+    const json = await ft.json();
+    console.log("data", json);
+    setData(json);
+  }
+  const {enViData } =data;
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container fixed className={classes.container}>
+      <div>
+        <TextField
+          id="standard-basic"
+          className={classes.textField}
+          label="Standard"
+          margin="normal"
+          value={keyword}
+          onChange={(e) =>setKeyword(e.target.value) }
+        />
+      </div>
+      <Button variant="contained" color="primary" onClick={() => search(keyword)}>
+        Search
+      </Button>
+      <div>
+      <Typography variant="h1" component="h2">
+        {enViData && enViData.best  && enViData.best.word}
+      </Typography>
+      </div>
+      </Container>
+    </React.Fragment>
   );
 }
 
