864df19c7e9f53aa95a6e759c6a238d83dda9428
Truong Nguyen <samuraitruong@hotmail.com>
2019-11-12 23:08:44 +1100

    Updates

---
+++ precache-manifest.a7dec7738c0de528dc2a306c1a80393a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50f82bea3da87d39682949777d641b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e610f24606ca64be6af0",
+    "url": "/static/css/main.5ecd223a.chunk.css"
+  },
+  {
+    "revision": "4c688e40aa1526894097",
+    "url": "/static/js/2.755db0c9.chunk.js"
+  },
+  {
+    "revision": "e610f24606ca64be6af0",
+    "url": "/static/js/main.42567568.chunk.js"
+  },
+  {
+    "revision": "903e041d4aa6af34f1ca",
+    "url": "/static/js/runtime-main.5f0d03dd.js"
+  }
+]);
