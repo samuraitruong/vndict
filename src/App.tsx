0790383379d4607514aad82fd5ae8be5e9372813
Truong Nguyen <samuraitruong@hotmail.com>
2019-11-12 23:02:03 +1100

    update layouts

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import { Box, Grid, Paper, IconButton, InputBase, Divider } from "@material-ui/core";
+import { Box, Grid, Paper, IconButton, InputBase, Divider, Chip } from "@material-ui/core";
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import DirectionsIcon from '@material-ui/icons/Directions';
@@ -13,6 +13,8 @@ import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
 import TranslateIcon from '@material-ui/icons/Translate';
 import SwapHorizIcon from '@material-ui/icons/SwapHoriz';
 import AccountTreeIcon from '@material-ui/icons/AccountTree';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
+import VolumeUpIcon from '@material-ui/icons/VolumeUp';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -30,28 +32,32 @@ const useStyles = makeStyles((theme: Theme) =>
       padding: 10,
     },
     divider: {
-      height: 28,
-      margin: 4,
+      height: 1,
+      marginTop: theme.spacing(1),
     },
     container: {
       margin: theme.spacing(1, 0),
     },
+    speakButton: {
+      marginLeft: theme.spacing(3)
+    }
   }),
 );
 
 
 const App: React.FC = () => {
   const classes = useStyles({});
-  const [data, setData] = useState({ en_vn: { data: null }, });
+  const [data, setData] = useState({ });
   const [keyword, setKeyword] = useState("");
   const [type, setType] = useState("en_vn");
   const search = async (inputKeyword: string) => {
-    const ft = await fetch(`https://samuraitruong.github.io/open-vn-en-dict/html/${inputKeyword}.json`);
+    const ft = await fetch(`https://samuraitruong.github.io/open-vn-en-dict/html/${inputKeyword.toLocaleLowerCase()}.json`);
     const json = await ft.json();
-    console.log("data", json);
+    console.log("datasource", json);
+    setType("en_vn")
     setData(json);
   }
-  const { en_vn } = data;
+  const dict = (data as any)[type];
   const handleTypeChange = (event: React.MouseEvent<HTMLElement>, newAlignment: string) => {
     setType(newAlignment);
   };
@@ -60,6 +66,11 @@ const App: React.FC = () => {
     e.preventDefault()
     return  false;
   }
+  const playSound = (word?: string , accent? : "us"| "uk")=> {
+    const url =`https://samuraitruong.github.io/open-vn-en-dict/voice/${word.toLocaleLowerCase()}_${accent}.mp3`
+    const audio = new Audio(url);
+    audio.play();
+  } 
   return (
     <React.Fragment>
       <CssBaseline />
@@ -95,7 +106,7 @@ const App: React.FC = () => {
               </Paper>
             </form>
           </Grid>
-          <Grid item sm={6} justify="space-between" alignContent="flex-end" alignItems="flex-end">
+          <Grid item sm={6}>
             <ToggleButtonGroup
               value={type}
               exclusive
@@ -106,8 +117,8 @@ const App: React.FC = () => {
               <ToggleButton value="en_vn" aria-label="left aligned">
                 <TranslateIcon></TranslateIcon> Eng -> Vi &nbsp;
               </ToggleButton>
-              <ToggleButton value="vn_en" aria-label="centered">
-                <SwapHorizIcon /> Vi -> Eng &nbsp;
+              <ToggleButton value="en_en" aria-label="centered">
+                <SwapHorizIcon /> Eng -> Eng &nbsp;
               </ToggleButton>
               <ToggleButton value="synonym" aria-label="right aligned">
                 <AccountTreeIcon />  Đồng Nghĩa
@@ -118,15 +129,37 @@ const App: React.FC = () => {
         </Grid>
 
 
-        <div>
-          <Typography variant="h4" component="h4">
-            {en_vn != null && en_vn.data ? en_vn.data.word : ""}
+        { dict && (
+        <Grid>
+          <Typography variant="h3" component="span">
+            {dict.data ? dict.data.word : ""}
+          </Typography>
+          <Typography variant="h4" component="span" >
+            <Box color="grey" component="span">
+            {dict.data ? `(${dict.data.pronounce})`: ""}
+            </Box>
+          </Typography>
+          <Box component="span" className="float-right">
+            <Chip className={classes.speakButton}
+              clickable
+              color="primary"
+              onClick={() => playSound(dict.data.word, "us")}
+              icon={<VolumeDownIcon />}
+              label="US"/>
+              <Chip
+              className={classes.speakButton}
+              clickable
+              color="secondary"
+              onClick={() => playSound(dict.data.word, "uk")}
+              icon={<VolumeUpIcon />}
+              label="UK"/>
+            </Box>
+          <Divider  className={classes.divider}/>
+          <Typography variant="body1" component="body" dangerouslySetInnerHTML={{ __html: dict.data ? dict.data.content : "" }}>
           </Typography>
 
-          <Typography variant="body1" component="body" dangerouslySetInnerHTML={{ __html: en_vn != null && en_vn.data ? en_vn.data.content : "" }}>
-          </Typography>
-
-        </div>
+        </Grid>
+        )}
       </Container>
     </React.Fragment >
   );
