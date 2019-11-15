9d95e1dd4873158e50d19f27a110124fca0682ab
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-15 05:32:08 +0000

    Deploy samuraitruong/vndict to github.com/samuraitruong/vndict.git:gh-pages

---
+++ precache-manifest.a935c95f19871e70c4bce73297766854.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "807e109dd804cbc2295c849af63042af",
+    "url": "/vndict/index.html"
+  },
+  {
+    "revision": "746109db0402266e90e1",
+    "url": "/vndict/static/css/main.ca370b12.chunk.css"
+  },
+  {
+    "revision": "4a4cf8616e2557405c94",
+    "url": "/vndict/static/js/2.5877ffaf.chunk.js"
+  },
+  {
+    "revision": "746109db0402266e90e1",
+    "url": "/vndict/static/js/main.4b1e11a8.chunk.js"
+  },
+  {
+    "revision": "b53ec20e2a82da325ab8",
+    "url": "/vndict/static/js/runtime-main.1f606cc8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vndict/precache-manifest.1a87be19fad2e27e999c3a2f01f563f8.js"
+  "/vndict/precache-manifest.a935c95f19871e70c4bce73297766854.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vndict/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
