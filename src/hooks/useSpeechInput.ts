bd12262b7e5e8f32dd701b4a8ec053c688ad8229
Truong Nguyen <truong.nguyen@team.telstra.com>
2019-11-22 11:19:57 +1100

    fix bug crash when browser not support speech recognition

--- src/hooks/useSpeechInput.ts
+++ src/hooks/useSpeechInput.ts
@@ -2,61 +2,65 @@ import { useState, useEffect, useCallback } from "react";
 // TODO move this to inside hook???
 const w = window as any;
 const SpeechRecognition = w.webkitSpeechRecognition ||
-                                    w.mozSpeechRecognition ||
-                                    w.msSpeechRecognition ||
-                                    w.oSpeechRecognition ||
-                                    w.SpeechRecognition;
+  w.mozSpeechRecognition ||
+  w.msSpeechRecognition ||
+  w.oSpeechRecognition ||
+  w.SpeechRecognition;
 
-const recognition = new SpeechRecognition();
+const recognition = SpeechRecognition && new SpeechRecognition();
 
-const useSpeechInput = (onVoiceInput: (w:string) => void, onVoiceStarted?: () => void) =>{
-    const isBrowserSupportSpeech = SpeechRecognition != null;
-    const [inputValue, setInputValue] = useState("");
-    const [started, setStarted] = useState(false);
-    
-    const startVoiceInput  = () => {
-      
-      recognition.start();
+const useSpeechInput = (onVoiceInput: (w: string) => void, onVoiceStarted?: () => void) => {
+  const isBrowserSupportSpeech = SpeechRecognition !== null && SpeechRecognition !== undefined;
+  const [inputValue, setInputValue] = useState("");
+  const [started, setStarted] = useState(false);
 
+  const startVoiceInput = () => {
+    if (!recognition) return;
+    recognition.start();
+
+  }
+  const voiceCallback = useCallback(() => {
+    if (!recognition) {
+      return;
     }
-    const voiceCallback  = useCallback(() => {
-      recognition.continuous = false;
-      recognition.interimResults = false;
-      recognition.lang = "en-US";
+    recognition.continuous = false;
+    recognition.interimResults = false;
+    recognition.lang = "en-US";
 
-      recognition.onresult = (e:any) => {
-        setInputValue(e.results[0][0].transcript);
-        onVoiceInput(e.results[0][0].transcript)
-        recognition.stop();
-      };
+    recognition.onresult = (e: any) => {
+      setInputValue(e.results[0][0].transcript);
+      onVoiceInput(e.results[0][0].transcript)
+      recognition.stop();
+    };
 
-      recognition.onaudiostart = ()=> {
-        if(onVoiceStarted) onVoiceStarted();
-        setStarted(true);
-      }
-       recognition.onend = ()=> {
-         setStarted(false);
-       }
+    recognition.onaudiostart = () => {
+      if (onVoiceStarted) onVoiceStarted();
+      setStarted(true);
+    }
+    recognition.onend = () => {
+      setStarted(false);
+    }
 
-      recognition.onerror = (e: any) =>  {
-        recognition.stop();
-      }
+    recognition.onerror = (e: any) => {
+      recognition.stop();
+    }
 
-    }, [onVoiceInput, onVoiceStarted])
-    useEffect(() => {
-      voiceCallback()
-      return () => {
-          recognition.stop()
-      }
+  }, [onVoiceInput, onVoiceStarted])
+  useEffect(() => {
+    voiceCallback()
+    return () => {
+      if (recognition && started)
+        recognition.stop()
+    }
 
-    },[voiceCallback])
+  }, [voiceCallback, started])
 
-    return {
-      started,
-        inputValue,
-        startVoiceInput,
-        isBrowserSupportSpeech
-    } 
+  return {
+    started,
+    inputValue,
+    startVoiceInput,
+    isBrowserSupportSpeech
+  }
 }
 
 export default useSpeechInput;
