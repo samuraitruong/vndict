6f6c947190d61424fc52c3a4c73e305331f3942e
samuraitruong <samuraitruong@users.noreply.github.com>
2021-04-07 23:02:45 +0000

    deploy: dbf2fefb0798cb0409993e1509ff31eaa6f37673

---
+++ precache-manifest.930ba0435edb3c62a58068240a35ab2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db6cced7c50a02ccf4fc4e16b2421ce1",
+    "url": "/vndict/index.html"
+  },
+  {
+    "revision": "89f3cc198031bb66cdc0",
+    "url": "/vndict/static/css/main.c7837df7.chunk.css"
+  },
+  {
+    "revision": "d004c4efe09d6a7a0516",
+    "url": "/vndict/static/js/2.34c7443d.chunk.js"
+  },
+  {
+    "revision": "89f3cc198031bb66cdc0",
+    "url": "/vndict/static/js/main.619a3be0.chunk.js"
+  },
+  {
+    "revision": "b53ec20e2a82da325ab8",
+    "url": "/vndict/static/js/runtime-main.1f606cc8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2b32105184add660ff64e2e608dbd858.js"
+  "/vndict/precache-manifest.930ba0435edb3c62a58068240a35ab2f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vndict/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
